713283ef939107054e57ffc75522f9d255815819
indyandie <1058837+Indyandie@users.noreply.github.com>
2023-05-15 00:19:09 -0500

    more enums

--- easiest-rust/dev/09-enums.rs
+++ easiest-rust/dev/09-enums.rs
@@ -1,14 +1,20 @@
 enum ThingsInTheSky {
-    Sun,
-    Stars,
+    Sun(String),
+    Stars(String),
+}
+
+enum Mood {
+    Happy,
+    Angry,
+    Sad,
 }
 
 fn getsky(time: i32) -> ThingsInTheSky {
     use ThingsInTheSky::*;
 
     match time {
-        6..=18 => Sun,
-        _ => Stars
+        6..=18 => Sun("The Sun is in the sky.".to_string()),
+        _ => Stars("The stars are in the sky.".to_string())
     }
 }
 
@@ -16,13 +22,72 @@ fn printsky(state: &ThingsInTheSky) {
     use ThingsInTheSky::*;
 
     match state {
-        Sun => println!("The Sun is in the sky."),
-        Stars => println!("The stars are in the sky.")
+        Sun(desc) => println!("{}", desc),
+        Stars(desc) => println!("{}", desc)
     } 
 }
 
+fn getmoodlevel(mood: &Mood) -> u8 {
+    use Mood::*;
+
+    match mood {
+        Happy => 10,
+        Angry => 5,
+        Sad => 0
+    }
+}
+
+enum PowerLevels {
+    DoYouEvenLift,
+    Weak,
+    GardentAnt,
+    GroceryBags,
+    OverNineThousand = 90001,
+    OldManStrength,
+}
+
+enum Number {
+    U16(u16),
+    I32(i32)
+}
+
+fn makenumber(number: i32) -> Number {
+    use Number::*;
+    match number.is_positive() {
+        true => U16(number as u16),
+        false => I32(number as i32)
+    }
+}
+
 fn main() {
+    println!("# Variant Values\n");
+    use Number::*;
+    let some_numbers = vec![makenumber(-234), makenumber(23)];
+    for num in some_numbers {
+        match num {
+            U16(num) => println!("{} is a u16.", num),
+            I32(num) => println!("{} is a i32.", num),
+        }
+    }
+    println!("\n\n");
+
+    println!("# Variant Values\n");
+    use PowerLevels::*;
+    let powerlevels = vec![DoYouEvenLift, Weak, GardentAnt, GroceryBags, OverNineThousand, OldManStrength];
+    for level in powerlevels {
+        println!("PowerLevels variant {}", level as i32);
+    }
+    println!("\n\n");
+    
+    println!("# Sky Enums\n");
     let time = 4;
     let mysky = getsky(time);
     printsky(&mysky);
+    println!("\n\n");
+
+    println!("# Sky Enums\n");
+    let my_mood = Mood::Angry;
+    let my_mood_level = getmoodlevel(&my_mood);
+    println!("My current mood level is {}.", my_mood_level);
+    println!("\n\n");
 }
