15db1adce0145dc01789315bd5b1883930d04c95
indyandie <1058837+Indyandie@users.noreply.github.com>
2023-07-02 15:50:00 -0500

    dot deref code

---
+++ easiest-rust/dev/13-ref_dot_op.rs
@@ -0,0 +1,47 @@
+struct Item {
+    num: u8,
+}
+
+impl Item {
+    fn compare_num(&self, other_num: u8) {
+        println!(
+            "{} and {} are equal, {}.",
+            self.num,
+            other_num,
+            self.num == other_num
+        );
+    }
+}
+
+fn main() {
+    println!("# Dot Operator Dereferencing\n");
+
+    println!("\n## Dereferencing\n");
+    let num = 8;
+    let num_ref = &num;
+
+    // this will not compile, different types u8 and &u8
+    // println!("{}", num == num_ref);
+    println!("{}", num == *num_ref);
+
+    println!("\n## Dot Operator\n");
+    let item = Item { num: 8 };
+    let ref_item_num = &item.num; // &u8 type
+                                  // cannot compare &u8 and u8
+                                  // println!("{}", ref_item_num == 8);
+    println!("{}", *ref_item_num == 8);
+
+    //using the dot oparator
+    let ref_item_num_dot = &item;
+
+    // no ref required
+    println!("{}", ref_item_num_dot.num == 8);
+
+    println!("\n## Method\n");
+    let ref_item = &item;
+    let ref_item_deep = &ref_item;
+
+    item.compare_num(8);
+    ref_item.compare_num(8);
+    ref_item_deep.compare_num(8);
+}
