16869eb33022e310295495e811f58b4cb005f257
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-02-19 22:20:40 -0600

    dev: where

--- easiest-rust/dev/14-generic_types.rs
+++ easiest-rust/dev/14-generic_types.rs
@@ -28,6 +28,16 @@ fn compare_display<T: Display, U: Display + PartialOrd>(msg: T, num1: U, num2: U
     println!("\n\n{msg}\nIs {num1} greater than {num2}? {comparison}");
 }
 
+fn where_gen<T, U, V, W>(name: T, id: U, msg: V, list: W)
+where
+    T: Display,
+    U: Display,
+    V: Display,
+    W: Debug,
+{
+    println!("{name}\n\t{id}\n\t{msg}\n\t{list:?}")
+}
+
 fn main() {
     let num = gen_type(43);
     get_type(num);
@@ -54,9 +64,20 @@ fn main() {
 
     compare_display("Yo!", 324, 3);
     compare_display("Another comparison...", 324, 30000);
+
     let msg = "Yet another comparison with defined variables...";
     let num1: i8 = 10;
     let num2: i8 = 40;
 
-    compare_display(msg, num1, num2);
+    compare_display(msg, num1, num2); // generic rules: num1 and num2 must be the same type
+
+    // ----------------------------------------------------------
+    // where
+
+    let name = "Name";
+    let id = 123;
+    let msg = "Hello there!".to_string();
+    let list = vec![1, 2, 3, 4, 5, 123, 52];
+
+    where_gen(name, id, msg, list);
 }
