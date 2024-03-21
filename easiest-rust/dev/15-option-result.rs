13637c24b5ce3f8b6d5551683922cb26614c4208
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-03-21 01:10:43 -0500

    dev(Option): `.unwrap()', match value

--- easiest-rust/dev/15-option-result.rs
+++ easiest-rust/dev/15-option-result.rs
@@ -6,11 +6,30 @@ fn fifth_opt(val: Vec<i32>) -> Option<i32> {
     }
 }
 
+fn print_opts(opts: Vec<Option<i32>>) {
+    for opt in opts {
+        match opt {
+            Some(num) => println!("Some value: {num}"),
+            None => println!("Nothing"),
+        }
+    }
+}
+
 fn main() {
     let vec1 = vec![21, 32, 43];
     let vec2 = vec![5, 15, 25, 35, 45, 55];
     let vec1_opt = fifth_opt(vec1.clone());
     let vec2_opt = fifth_opt(vec2.clone());
 
-    print!("{vec1:?}\n{vec1_opt:?}\n\n{vec2:?}\n{vec2_opt:?}\n")
+    println!("# Option\n\n## Basic\n");
+    print!("{vec1:?}\n{vec1_opt:?}\n\n{vec2:?}\n{vec2_opt:?}\n\n---\n");
+
+    println!("## Option Values\n");
+    let unwrapped_opt = vec2_opt.unwrap();
+    println!("Unwrapped value: {unwrapped_opt}\n");
+
+    let mut vec_opts = Vec::new();
+    vec_opts.push(fifth_opt(vec1));
+    vec_opts.push(fifth_opt(vec2));
+    print_opts(vec_opts);
 }
