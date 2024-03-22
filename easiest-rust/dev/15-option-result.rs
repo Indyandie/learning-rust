85ceee2f44dc6994bd23344570da64c9308e69f9
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-03-21 22:36:29 -0500

    dev(Options): `is_some()`

--- easiest-rust/dev/15-option-result.rs
+++ easiest-rust/dev/15-option-result.rs
@@ -22,14 +22,28 @@ fn main() {
     let vec2_opt = fifth_opt(vec2.clone());
 
     println!("# Option\n\n## Basic\n");
-    print!("{vec1:?}\n{vec1_opt:?}\n\n{vec2:?}\n{vec2_opt:?}\n\n---\n");
+    print!("{vec1:?}\n{vec1_opt:?}\n\n{vec2:?}\n{vec2_opt:?}\n\n---\n\n");
 
     println!("## Option Values\n");
     let unwrapped_opt = vec2_opt.unwrap();
     println!("Unwrapped value: {unwrapped_opt}\n");
 
     let mut vec_opts = Vec::new();
-    vec_opts.push(fifth_opt(vec1));
-    vec_opts.push(fifth_opt(vec2));
+    vec_opts.push(fifth_opt(vec1.clone()));
+    vec_opts.push(fifth_opt(vec2.clone()));
     print_opts(vec_opts);
+
+    println!("\n---\n");
+    println!("\n## .is_some\n");
+
+    let vec_opts = vec![vec1, vec2];
+    for vc in vec_opts {
+        let vec_5 = fifth_opt(vc);
+
+        if vec_5.is_some() {
+            println!("is some unwrapped: {}", vec_5.unwrap());
+        } else {
+            println!("Nothing!");
+        }
+    }
 }
